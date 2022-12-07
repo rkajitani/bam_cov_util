0a5e49697066e541778dc9b551b3409aba2423f7
rkajitani <rei.kajitani@gmail.com>
2022-12-07 11:58:43 +0900

    add parameters of median coverage and edge length to coverage_breakpoint.cpp

--- coverage_breakpoint.cpp
+++ coverage_breakpoint.cpp
@@ -2,14 +2,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <ctype.h>
 
 using namespace std;
 
+int get_median(const vector<int>::iterator begin, const vector<int>::iterator end);
+
+
 int main(int argc, char **argv)
 {
-	if (argc != 3) {
-		cerr << "usage: " << argv[0] << " coverage.txt min_coverage\n";
+	if (argc != 5) {
+		cerr << "usage: " << argv[0] << " coverage.txt cov_th cov_median_th edge_len\n";
 		exit(1);
 	}
 
@@ -19,30 +23,60 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	const int min_cov = atoi(argv[2]);
+	const int cov_th = atol(argv[2]);
+	const int cov_med_th = atol(argv[3]);
+	const long edge_len = atol(argv[4]);
 
 	string line;
 	string name;
 	int pos;
-	vector<int> freq;
+	vector<int> cov;
 
 	while (!ifs.eof()) {
 		getline(ifs, line);
-		if (line.empty())
+		if (line.empty()) {
 			continue;
+		}
 
 		if (line[0] == '>') {
+			if (cov.size() > 2 * edge_len) {
+				int median = get_median(cov.begin() + edge_len, cov.end() - edge_len);
+				if (median >= cov_med_th) {
+					for (long i = edge_len; i < cov.size() - edge_len; ++i) {
+						if (cov[i] <= cov_th) {
+							cout << name << '\t' << i << '\n';
+						}
+					}
+				}
+			}
+
+			cov.clear();
 			name = line.substr(1);
-			pos = 0;
 		}
-		else{
-			if (atoi(line.c_str()) < min_cov)
-				cout << name << '\t' << pos << '\n';
-			++pos;
+		else {
+			cov.push_back(stoi(line));
+		}
+	}
+	if (cov.size() > 2 * edge_len) {
+		int median = get_median(cov.begin() + edge_len, cov.end() - edge_len);
+		if (median >= cov_med_th) {
+			for (long i = edge_len; i < cov.size() - edge_len; ++i) {
+				if (cov[i] <= cov_th) {
+					cout << name << '\t' << i << '\n';
+				}
+			}
 		}
 	}
 
 	ifs.close();
 	
 	return 0;
+}
+
+
+int get_median(const vector<int>::iterator begin, const vector<int>::iterator end)
+{
+	vector<int> vec(begin, end);
+    partial_sort(vec.begin(), vec.begin() + vec.size() / 2 + 1, vec.end());
+    return vec[vec.size() / 2];
 }
